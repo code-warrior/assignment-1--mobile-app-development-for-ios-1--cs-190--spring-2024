d601a8824af659e513f99dd7e27cbef8771dc851
Declan W <declanwallace878@gmail.com>
2024-03-26 15:03:30 -0400

    Created variables space and stars

--- main.swift
+++ main.swift
@@ -1,5 +1,7 @@
 func printDiamond(size: Int) {
-    if size % 2 != 0 {
+    if size % 2 != 0 { //Creating an odd diamond
         print("Diamond with size \(size):")
+        var space = size / 2 //space refers to the amount of spaces needed before going to the next line
+        var stars = 1 //the initial amount of asterisks on the first line
     }
 }
