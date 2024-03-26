05f4e1bd925085bf4fb2c1b759d21edcc9205f28
Declan W <declanwallace878@gmail.com>
2024-03-26 15:43:17 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,12 +1,17 @@
+//Create an odd diamond
 func printDiamond(size: Int) {
-    if size % 2 != 0 { //Creating an odd diamond
+    if size % 2 != 0 {
         print("Diamond with size \(size):")
-        var space = size / 2 //space refers to the amount of spaces needed before going to the next line
-        var stars = 1 //the initial amount of asterisks on the first line
+        var space = size / 2 //amount of spaces needed before going to the next line
+        var stars = 1 //amount of asterisks on the first line
+        //Create a for loop for the upper half of the diamond
         for _ in 0..<(size / 2 + 1) {
             print(string(repeating: " ", count: space) + string(repeating: "*", count: stars))
             space -= 1
             stars += 2
-        } //this for loop is meant for the top half of the diamond as it approaches the middle (users input)
+        }
+        //Create variables for the second for loop for bottom half
+        space = 1
+        stars = size - 2
         }
     }
