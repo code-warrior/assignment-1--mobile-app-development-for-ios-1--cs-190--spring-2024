db4a6c2e8b90a7d62127cf5092cd262e19aacc50
Declan W <declanwallace878@gmail.com>
2024-03-26 16:08:42 -0400

    Fixed formatting of comments

--- main.swift
+++ main.swift
@@ -1,27 +1,30 @@
-//Create an odd diamond
+//  Create an odd diamond
 func printDiamond(size: Int) {
     if size % 2 != 0 {
         print("Diamond with size \(size):")
-        var space = size / 2 //amount of spaces needed before going to the next line
-        var stars = 1 //amount of asterisks on the first line
-        //Create a for loop for the upper half of the diamond
+        var space = size / 2 //  amount of spaces needed before going to the next line
+        var stars = 1 //  amount of asterisks on the first line
+        //  Create a for loop for the upper half of the diamond
         for _ in 0..<(size / 2 + 1) {
             print(String(repeating: " ", count: space) + String(repeating: "*", count: stars))
             space -= 1
             stars += 2
         }
-        //Create variables for the second for loop for bottom half
+        //  Create variables for the second for loop for bottom half
         space = 1
         stars = size - 2
-        //Create a for loop for the bottom half of the diamond
+        //  Create a for loop for the bottom half of the diamond
         for _ in 0..<(size / 2) {
             print(String(repeating: " ", count: space) + String(repeating: "*", count: stars))
             space += 1
             stars -= 2
         }
+        //  Create an even diamond
+        } else {
+          print()
         }
     }
-    //Ask the user for their input and print the diamond
+    //  Ask the user for their input and print the diamond
 print("Enter the size of your diamond:")
 if let size = Int(readLine() ?? "") {
     printDiamond(size: size)
