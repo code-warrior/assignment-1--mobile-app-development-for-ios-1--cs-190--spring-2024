9716fbc2258a20cca4ae37b812b1358a1a882a59
Declan W <declanwallace878@gmail.com>
2024-03-26 15:17:08 -0400

    Update main.swift

--- main.swift
+++ main.swift
@@ -3,5 +3,10 @@ func printDiamond(size: Int) {
         print("Diamond with size \(size):")
         var space = size / 2 //space refers to the amount of spaces needed before going to the next line
         var stars = 1 //the initial amount of asterisks on the first line
+        for _ in 0..<(size / 2 + 1) {
+            print(string(repeating: " ", count: space) + string(repeating: "*", count: stars))
+            space -= 1
+            stars += 2
+        } //this for loop is meant for the top half of the diamond as it approaches the middle (users input)
+        }
     }
-}
