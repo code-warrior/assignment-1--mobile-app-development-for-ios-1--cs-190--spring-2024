48a9dd65981042dceae9cf55b18f0889a83858d0
Roy Vanegas <roy@thecodeeducators.com>
2024-04-08 14:47:53 -0400

    Suggested code updates

--- main.swift
+++ main.swift
@@ -1,10 +1,11 @@
 //  Create an odd diamond
 func printDiamond(size: Int) {
-
   if size % 2 != 0 {
-    print("Diamond with a size of \(size):")
     var space = size / 2 //  Amount of spaces needed before going to the next line
     var stars = 1 //  Amount of asterisks on the first line
+
+    print("Diamond with a size of \(size):")
+
     //  Create a for loop for the upper half of the diamond
     for _ in 0..<(size / 2 + 1) {
         print(String(repeating: " ", count: space) + String(repeating: "*", count: stars))
@@ -15,21 +16,20 @@ func printDiamond(size: Int) {
     //  Create variables for the second for loop for bottom half
     space = 1
     stars = size - 2
+
     //  Create a for loop for the bottom half of the diamond
     for _ in 0..<(size / 2) {
         print(String(repeating: " ", count: space) + String(repeating: "*", count: stars))
         space += 1
         stars -= 2
     }
-  }
-
-  //  Create an even diamond
-  else {
-      print("Diamond with a size of \(size):")
-      //  Print a singular asterisk before the for loop
-      print(String(repeating: " ", count: size - 1) + "* ", terminator: "")
+  } else { //  Create an even diamond
       var space = size / 2
       var stars = 1
+
+      print("Diamond with a size of \(size):")
+      print(String(repeating: " ", count: size - 1) + "* ", terminator: "")
+
       //  Create a for loop for the upper half of the diamond
       for _ in 0..<(size / 2) {
           print(String(repeating: " ", count: space * 2) + String(repeating: "* ", count: stars - 1))
@@ -44,12 +44,12 @@ func printDiamond(size: Int) {
           stars -= 2
       }
 
-      //  Print the singular asterisk at the bottom
       print(String(repeating: " ", count: size - 1) + "* ")
     }
 }
-//  Ask the user for their input and print the diamond
+
 print("Enter the size of your diamond:")
+
 if let size = Int(readLine() ?? "") {
     printDiamond(size: size)
 } else {
